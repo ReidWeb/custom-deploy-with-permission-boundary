fbda30b35cad5b0805727718c37698ccd3f6a4b3
Peter Reid <peter@reidweb.com>
2021-07-17 18:14:22 +0100

    feat(iam): Apply permission boundary to stack and break deployment
    Signed-off-by: Peter Reid <peter@reidweb.com>

--- lib/custom-deploy-with-permission-boundary-stack.ts
+++ lib/custom-deploy-with-permission-boundary-stack.ts
@@ -2,12 +2,25 @@ import * as cdk from '@aws-cdk/core';
 import {CfnOutput, CfnOutputProps} from '@aws-cdk/core';
 import {Bucket, BucketEncryption, BucketProps} from "@aws-cdk/aws-s3";
 import {BucketDeployment, BucketDeploymentProps, Source} from "@aws-cdk/aws-s3-deployment";
+import {PermissionsBoundary} from "@aws-cdk/aws-iam/lib/permissions-boundary";
+import {ManagedPolicy, PolicyDocument} from "@aws-cdk/aws-iam";
+import * as fs from "fs";
 
 export class CustomDeployWithPermissionBoundaryStack extends cdk.Stack {
 	constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props)
 
 		// The code that defines your stack goes here
+
+		const rawData = fs.readFileSync('./lib/boundary-policy.json')
+		// @ts-ignore
+		const policyJson = JSON.parse(rawData)
+
+		const boundary = new ManagedPolicy(this, 'Boundary', {
+			document: PolicyDocument.fromJson(policyJson)
+		});
+		PermissionsBoundary.of(this).apply(boundary);
+
 
 		const bucketConfig: BucketProps = {
 			websiteIndexDocument: 'index.html',
